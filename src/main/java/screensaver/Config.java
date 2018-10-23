7bcfe95b1542bf85401e01f5a5d1fe5ec9a04e21
freeernest <erikfeigin@gmail.com>
2018-10-23 07:21:51 +0300

    Updated till 36:16 part 2

--- src/main/java/screensaver/Config.java
+++ src/main/java/screensaver/Config.java
@@ -1,7 +1,21 @@
 package screensaver;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+import java.awt.*;
+import java.util.Random;
+
 /**
  * Created by Erik Feigin on 21/10/2018.
  */
+@Configuration
+@ComponentScan(basePackages = "screensaver")
 public class Config {
+    @Bean
+    public Color color(){
+        Random random = new Random();
+        return new Color(random.nextInt(255), random.nextInt(255), random.nextInt(255));
+    }
 }
